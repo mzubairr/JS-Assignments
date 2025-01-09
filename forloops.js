4e0186e28cd3dc9a5e33b23b031fdc427a024cea
mzubairr <mzubair5979@gmail.com>
2025-01-09 15:35:52 +0500

    for loops assignment started

---
+++ forloops.js
@@ -0,0 +1,22 @@
+// for loops.
+
+// 1. Write a program to display the message "Hello World" in your browser using for loop.
+let display = document.querySelector("#display");
+// for (let i = 1; i <= 5; i++) {
+//   display.innerHTML += `Hello World <br/>`;
+// }
+
+// 2. Write a program to print numeric counting from 1 to 10.
+// for (let i = 1; i <= 10; i++){
+//     display.innerHTML += `${i} <br/>`;
+// }
+
+// 3. Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input from user
+// let tableNo = +prompt("Enter a number to print its multiplication table");
+// let tablelength = +prompt("Enter length of multiplication table");
+// display.innerHTML += `Multiplication table of ${tableNo} <br/>`;
+// display.innerHTML += `Length ${tablelength} <br/><br/>`;
+// for (let i = 1; i <= tablelength; i++) {
+//   display.innerHTML += `${tableNo} x ${i} = ${tableNo * i} <br/> `;
+// }
+
