981a7de53956fdf14ef809b9d082c3642af97b5d
mzubairr <mzubair5979@gmail.com>
2025-01-10 16:11:52 +0500

    arrays started

---
+++ arrays.js
@@ -0,0 +1,12 @@
+// Arrays
+
+// 1)  Declare an empty array using JS literal notation to store student names in future.
+
+// let students = [];
+// console.log(students);
+
+// 2) Declare an empty array using JS object notation student names in future.
+// let studentsData = {
+//     studentNames: []
+// };
+// console.log(studentsData);
